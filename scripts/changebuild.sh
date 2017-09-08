2b6271779f9a90ede92c19a4cd1c2c9e512dc526
NICOLAS Yves <yves.nicolas@soprasteria.com>
2017-09-08 20:41:09 +0200

    feat: updated changebuild.sh

--- scripts/changebuild.sh
+++ scripts/changebuild.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# a script to change the build which runs for a specfic container
+# a script to change the build which runs for a specfic service
 # usage changeBuild <service> <git-sha>
+# make sure you update variables at the begining of the script
+
+dockerRepo='yvnicolas/digibox'
+nginxnet='nginx-net'
 
 function usage
 {
- echo "changebuild.sh [OPTIONS] <service> <buildGitSha>"
+ echo "changebuild.sh [OPTIONS] <service> <dockerImageTag>"
  echo "   Change running build in dynamease production environment"
  echo "  OPTIONS"
  echo "    -o : additionnal docker options that will be inserted in the docker run command"
  echo "    -h : help"
 
 }
+
 
 # find pre running tomcat container
 function identify_container
@@ -24,7 +29,8 @@ function identify_container
        previouscontainer=$(docker ps -a | grep "$service"- | awk '{print $NF}' )
        echo "previous container $previouscontainer will be stopped and logged"
     else
-      echo "Found $nbrContainer containers matching service $service, you will have to live with that or destroy them manually"
+      echo "Found $nbrContainer containers matching service $service, please clean the previous one and start again"
+      exit 1
     fi
 }
 
@@ -43,6 +49,8 @@ while getopts 'ho:' flag; do
 	  exit 0;;
     o) dockeroptions="${OPTARG}" ;
        argindex=$(($argindex+2));;
+       d) dockerRepo="${OPTARG}" ;
+          argindex=$(($argindex+2));;
     *) error "Unexpected option ${flag}"
        usage
        ;;
@@ -73,8 +81,8 @@ newcontainer="$service"-"$gitsha"
 identify_container
 
 # start new container
-echo "starting tomcat container $newcontainer with additionnal docker options $dockeroptions"
-docker run -d --name "$newcontainer"  $dockeroptions yvnicolas/$service:$gitsha
+echo "starting service container $newcontainer with additionnal docker options $dockeroptions"
+docker run -d --name "$newcontainer"  $dockeroptions $dockerRepo:$gitsha
 
 # wait for it to properly start
 echo "$newcontainer"" started, waiting for it to be deployed"
@@ -82,12 +90,15 @@ sleep 30
 
 # Activate it connecting the new container to the nginxnetwork
 echo "switching ""$service"" release thru Nginx network connection"
-docker network connect --alias $service nginxnet $newcontainer
+if [ $previouscontainer != '' ];then
+  docker network disconnect $nginxnet $previouscontainer
+fi
+docker network connect --alias $service $nginxnet $newcontainer
 
 # Stop old tomcat container and take logs for it
 if [ $previouscontainer != '' ];then
 
-  echo "stopping previous Tomcat container  $previouscontainer"
+  echo "stopping previous service container  $previouscontainer"
 #  docker stop "$previouscontainer"
   echo "archiving logs to $HOME/logs/$service-$gitsha-`date +%Y%m%d-%H%M`"
   docker logs "$previouscontainer" > "$HOME"/logs/"$service"-"$gitsha"-`date +%Y%m%d-%H%M`
